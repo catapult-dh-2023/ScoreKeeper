5a69e186d58cd560516612191f0dd50099485afb
sizetokage <moriyama.keita0015@gmail.com>
2023-10-12 17:52:10 +0900

    Seederファイルの作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Game;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,40 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //Userテーブル(1件)
+         \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+         ]);
+
+        //Gameテーブル(1件)
+         \App\Models\Game::create([
+             'date' => '2023-10-12',
+             'event_name' => 'TestEvent',
+             'situation' => 'TestSituation',
+             'total_participants' => 100000,
+             'note' => 'Gameidが発行されてない個人成績はこのGaemとして扱いましょう' 
+         ]);
+
+        //roleテーブル(たくさん)
+         \App\Models\Role::create([
+             'name' => '人狼',
+         ]);
+         \App\Models\Role::create([
+             'name' => '市民',
+         ]);
+        
+        //resultテーブル(２件)
+         \App\Models\Result::create([
+             'name' => '勝ち',
+             
+         ]);
+         \App\Models\Result::create([
+             'name' => '負け', 
+         ]);
+
+         \App\Models\ResultWay::create([
+             'name' => '吊られ',
+         ]);
     }
 }
