8dcf689c0a1a5ad12238172e39eae6f52085b650
Taichi <takatobi002@gmail.com>
2023-10-20 18:44:17 +0900

    LINEログインの実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable(); // 変更
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable(); // 変更
+            $table->string('line_user_id')->unique()->nullable(); // 追加
             $table->rememberToken();
             $table->timestamps();
         });
